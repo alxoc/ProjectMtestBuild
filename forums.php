68ab0db54af70209d22f6da97b59fcbfb7b6ed5c
alxoc <162996@student.horizoncollege.nl>
2021-12-13 08:39:59 +0100

    Bug fixes

--- forums.php
+++ forums.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Project M</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="styles/style.css">
+
+        <title>Project M</title>
+    </head>
     <body>
         <header>
             <div class="headerContainer">
                 <div class="headerLogo"><p>Project M</p></div>
                 <div class="topnav" id="myTopnav">
                     <a href="logout.php">Log out</a>
-                    <a href="forums.php">Forums</a>
+                    <a class="active" href="forums.php">Forums</a>
                     <a href="#">Document hub</a>
                     <a href="#">Information</a>
-                    <a class="active" href="main.php">Home</a>
+                    <a href="main.php">Home</a>
                     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                         <i class="fa fa-bars"></i>
                     </a>
@@ -26,15 +27,7 @@
         </header>
 
         <main>
-            <div class="mainContainer">
-                <div class="navigationContainer">
-                    <div class="navigationTitle"><p>Welcome to Project M</p></div>
-                    <div class="navigationUnderTitle"><p>Alex(placeholder)</p></div>
-                    <div class="selector"><a href="forums.php">Forums</a></div>
-                    <div class="selector"><a href="#">Document hub</a></div>
-                    <div class="selector"><a href="#">Information</a></div>
-                </div>
-            </div>
+            
         </main>
         <?php
         session_start();
